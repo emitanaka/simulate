10861f532ace68c8f8cd2951feca179bd008ab68
Emi <dr.emi.tanaka@gmail.com>
2022-01-05 13:35:50 +1100

    .upper and .lower for the correlation_matrix

--- R/correlate.R
+++ R/correlate.R
@@ -1,47 +1,72 @@
 
-#' Generate correlated random variables
-#'
-#' @export
-correlate <- function(.data, vars = NULL, mean = NULL,
-                      cor = NULL, cov = NULL, dist = "normal") {
-  comat <- cor %||% cov
-  p <- nrow(comat)
-  n <- nrow(.data)
-  mean <- mean %||% rep(0, p)
-  if(all(dist=="normal")) {
-    eS <- eigen(comat, symmetric = TRUE)
-    ev <- eS$values
-    out <- mean + eS$vectors %*% diag(sqrt(ev)) %*% t(matrix(rnorm(p * n), n))
-    out <- t(out)
-    colnames(out) <- vars
-  }
-  else {
-    abort("Not implemented")
-  }
-  cbind(.data, out)
-}
+#' #' Generate correlated random variables
+#' #'
+#' #' @export
+#' correlate <- function(.data, vars = NULL, mean = NULL,
+#'                       cor = NULL, cov = NULL, dist = "normal") {
+#'   comat <- cor %||% cov
+#'   p <- nrow(comat)
+#'   n <- nrow(.data)
+#'   mean <- mean %||% rep(0, p)
+#'   if(all(dist=="normal")) {
+#'     eS <- eigen(comat, symmetric = TRUE)
+#'     ev <- eS$values
+#'     out <- mean + eS$vectors %*% diag(sqrt(ev)) %*% t(matrix(rnorm(p * n), n))
+#'     out <- t(out)
+#'     colnames(out) <- vars
+#'   }
+#'   else {
+#'     abort("Not implemented")
+#'   }
+#'   cbind(.data, out)
+#' }
 
 #' Convert values to correlation matrix
 #'
+#' @param ... A series of values or dots for the correlation matrix.
+#' @param .names The name of the variables.
+#' @param .upper,.lower A vector of values for the upper (lower) triangle values
+#'   filled by column (row). If the `.upper` or `.lower` is defined then `...`
+#'   must not be defined.
+#'
 #' @export
-correlation_matrix <- function(..., .names = NULL) {
-  dots <- enquos(...)
-  n <- length(dots)
-  nvar <- sqrt(n)
-  stopifnot(nvar %% 1 == 0)
+correlation_matrix <- function(..., .names = NULL, .upper = NULL, .lower = NULL) {
 
-  x <- which(!map_lgl(dots, is_a_dot))
-  vals <- map_dbl(dots[x], eval_tidy)
+  if(!is.null(.upper)) {
+    nvec <- length(.upper)
+    nvar <- which(triangular(nvec) == nvec) + 1
+    x <- which(as.vector(upper.tri(diag(nvar), diag = FALSE)))
+    vals <- .upper
+
+  } else if(!is.null(.lower)) {
+    nvec <- length(.lower)
+    nvar <- which(triangular(nvec) == nvec) + 1
+    x <- which(as.vector(t(lower.tri(diag(nvar), diag = FALSE))))
+    vals <- .lower
+
+  } else {
+    dots <- enquos(...)
+    n <- length(dots)
+    nvar <- sqrt(n)
+    x <- which(!map_lgl(dots, is_a_dot))
+    vals <- map_dbl(dots[x], eval_tidy)
+    ilower <- which(as.vector(lower.tri(diag(nvar), diag = FALSE)))
+    iupper <- which(as.vector(upper.tri(diag(nvar), diag = FALSE)))
+    stopifnot(all(x %in% ilower) | all(x %in% iupper))
+  }
+
+  stopifnot(nvar %% 1 == 0)
+  stopifnot(all(vals <= 1) & all(vals >= -1))
 
   out <- 0.5 * diag(nvar)
-  ilower <- which(as.vector(lower.tri(diag(nvar), diag = FALSE)))
-  iupper <- which(as.vector(upper.tri(diag(nvar), diag = FALSE)))
-  stopifnot(all(x %in% ilower) | all(x %in% iupper))
+
   out[x] <- vals
   out <- out + t(out)
   colnames(out) <- rownames(out) <- .names
   out
 }
+
+triangular <- function(n) sapply(1:n, function(x) sum(1:x))
 
 is_a_dot <- function(x) is_symbol(get_expr(x), name = ".")
 
