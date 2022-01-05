599693926032b5109bdfc5458e5250d784507692
Emi <dr.emi.tanaka@gmail.com>
2022-01-05 13:36:00 +1100

    testing framework update

--- tests/testthat/test-correlate.R
+++ tests/testthat/test-correlate.R
@@ -1,15 +1,76 @@
 test_that("correlation", {
-  cormat(., 0, 0.5,
-         ., ., .,
-         ., ., .)
+  expect_true({
+    correlation_matrix(., runif(1, -1, 1), runif(1, -1, 1),
+                       ., ., runif(1, -1, 1),
+                       ., ., .) %>%
+      isSymmetric()
+  })
 
-  x <- 0.3
-  cormat(., ., x,
-         ., ., .,
-         ., ., .)
+  expect_true({
+    correlation_matrix(., ., .,
+                       runif(1, -1, 1), ., .,
+                       runif(1, -1, 1), runif(1, -1, 1), .) %>%
+      isSymmetric()
+  })
 
-  cormat(., ., 1/9, .,
-         ., ., ., .,
-         ., ., ., .,
-         ., ., ., .)
+  expect_equal({
+    correlation_matrix(., 0, 0.5,
+                       ., ., .,
+                       ., ., .)
+  }, matrix(c(1, 0, 0.5, 0, 1, 0, 0.5, 0, 1), nrow = 3))
+
+  expect_error({
+    correlation_matrix(., 0, -2,
+                       ., ., .,
+                       ., ., .)
+  })
+
+  expect_error({
+    correlation_matrix(., 0, 0.8,
+                       ., ., .,
+                       -0.8, ., .)
+  })
+
+  expect_error({
+    correlation_matrix(., 0, 0.8,
+                       ., ., .,
+                       -0.8, ., ., .)
+  })
+
+  expect_equal({
+    correlation_matrix(., 0, 0.5,
+                       ., ., .,
+                       ., ., .)
+  }, matrix(c(1, 0, 0.5, 0, 1, 0, 0.5, 0, 1), nrow = 3))
+
+  expect_equal({
+    correlation_matrix(., 0, -1,
+                       ., ., .,
+                       ., ., .)
+  }, matrix(c(1, 0, -1, 0, 1, 0, -1, 0, 1), nrow = 3))
+
+  expect_equal({
+    x <- 0.3
+    correlation_matrix(., 0, x,
+                       ., ., .,
+                       ., ., .)
+  }, matrix(c(1, 0, 0.3, 0, 1, 0, 0.3, 0, 1), nrow = 3))
+
+  expect_equal({
+    correlation_matrix(., 0, 1/9,
+                       ., ., .,
+                       ., ., .)
+  }, matrix(c(1, 0, 1/9, 0, 1, 0, 1/9, 0, 1), nrow = 3))
+
+
+
+  expect_equal({
+    correlation_matrix(.upper = c(0.4, 0.5, 0.8, 0.1, 0.2, 0.3))
+  }, matrix(c(1, 0.4, 0.5, 0.1, 0.4, 1, 0.8, 0.2, 0.5, 0.8, 1, 0.3, 0.1, 0.2, 0.3, 1), nrow = 4))
+
+  expect_equal({
+    # FIXME
+    correlation_matrix(.lower = c(0.4, 0.5, 0.8, 0.1, 0.2, 0.3))
+  }, matrix(c(1, 0.4, 0.5, 0.8, 0.4, 1, 0.1, 0.2, 0.5, 0.1, 1, 0.3, 0.8, 0.2, 0.3, 1), nrow = 4, byrow = TRUE))
+
 })
--- tests/testthat/test-simulate.R
+++ tests/testthat/test-simulate.R
@@ -2,9 +2,8 @@ test_that("simulate", {
   g1 <- sample(c("a", "b"), 20000, replace = TRUE)
   g2 <- sample(c("a", "b"), 20000, replace = TRUE)
   expect_equal({
-    sim_form(~ p[1] * g1 +
-               p[2] * g2 +
-               p[3] * g3, data = data.frame(g1 = g1, g2 = g2, g3 = g2)) %>%
+    sim_form(~ p[1] * g1 + p[2] * g2 + p[3] * g3,
+             data = data.frame(g1 = g1, g2 = g2, g3 = g2)) %>%
       params(g1 = c(1, 1),
              g2 = c(2, 2),
              g3 = c(3, 3),
@@ -27,24 +26,6 @@ test_that("simulate", {
              p = sim_bernoulli(0)) %>%
       simulate()
   }, 0, ignore_attr = TRUE)
-
-
-  df1 <- data.frame(grp1 = c("C", "A", "A", "A", "C", "C", "B", "B", "B", "A", "B", "B",
-                   "A", "C", "C", "C", "B", "C", "B", "B", "A", "C", "B", "A", "C",
-                   "A", "A", "A", "C", "C", "C", "B", "C", "C", "B", "B", "B", "C",
-                   "A", "C", "A", "A", "B", "C", "B", "A", "C", "B", "B", "A", "A",
-                   "B", "B", "B", "B", "A", "C", "A", "C", "B", "C", "A", "B", "C",
-                   "A", "B", "C", "A", "C", "C", "C", "B", "B", "B", "C", "B", "C",
-                   "C", "B", "A", "C", "C", "A", "A", "B", "B", "C", "B", "B", "B",
-                   "A", "C", "A", "A", "B", "C", "B", "C", "C", "C"),
-             grp2 = c("e", "g", "c", "c", "c", "e", "a", "e", "a", "g", "a", "c",
-                      "a", "g", "b", "f", "g", "b", "f", "g", "d", "a", "g", "e", "d",
-                      "d", "d", "d", "b", "g", "a", "g", "e", "b", "e", "b", "g", "e",
-                      "f", "e", "d", "a", "a", "g", "b", "g", "e", "c", "f", "d", "b",
-                      "e", "c", "b", "b", "b", "g", "b", "a", "d", "g", "g", "f", "a",
-                      "c", "c", "c", "a", "a", "c", "b", "e", "a", "e", "d", "d", "f",
-                      "c", "g", "g", "c", "g", "g", "c", "e", "c", "b", "c", "d", "c",
-                      "a", "b", "f", "f", "d", "f", "b", "e", "d", "d"))
 
 
   cormat(., 0, 0.5,
