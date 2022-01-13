b64481e132119e8fd8e463056dbbd41dde771bc4
Emi <dr.emi.tanaka@gmail.com>
2022-01-13 11:46:20 +1100

    save_seed updated so it actually works

--- R/simulate.R
+++ R/simulate.R
@@ -243,10 +243,8 @@ save_seed <- function(seed) {
   if (is.null(seed))
     RNGstate <- get(".Random.seed", envir = .GlobalEnv)
   else {
-    R.seed <- get(".Random.seed", envir = .GlobalEnv)
     set.seed(seed)
     RNGstate <- structure(seed, kind = as.list(RNGkind()))
-    on.exit(assign(".Random.seed", R.seed, envir = .GlobalEnv))
   }
   RNGstate
 }
